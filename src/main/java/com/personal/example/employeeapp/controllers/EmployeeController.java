47cdd36d220c977d1036d6f20e41c1b9c8d1f715
Neil McDonagh <neilmcdonagh@Neils-MacBook-Pro.local>
2019-12-27 15:18:25 +0000

    json data returned for entry in db

--- src/main/java/com/personal/example/employeeapp/controllers/EmployeeController.java
+++ src/main/java/com/personal/example/employeeapp/controllers/EmployeeController.java
@@ -1,4 +1,28 @@
 package com.personal.example.employeeapp.controllers;
+import com.personal.example.employeeapp.models.Employee;
+import com.personal.example.employeeapp.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/employees")
 public class EmployeeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Employee> getEmployee(@PathVariable Long id) {
+        return employeeRepository.findById(id);
+    }
 }
