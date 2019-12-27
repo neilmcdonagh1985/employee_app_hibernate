70a365445bb860b78b1a9506a34feddea2fb83e6
Neil McDonagh <neilmcdonagh@Neils-MacBook-Pro.local>
2019-12-27 12:59:40 +0000

    initial set-up

---
+++ src/main/java/com/personal/example/employeeapp/models/Employee.java
@@ -0,0 +1,61 @@
+package com.personal.example.employeeapp.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+
+    public Employee(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Employee() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
