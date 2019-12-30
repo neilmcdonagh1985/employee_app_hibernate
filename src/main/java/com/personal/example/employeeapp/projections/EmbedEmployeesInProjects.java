cf131b368b0fb0fd5de78e6d99cdda2a0a316d03
Neil McDonagh <neilmcdonagh@Neils-MacBook-Pro.local>
2019-12-30 11:25:05 +0000

    projection for employees in project created

--- src/main/java/com/personal/example/employeeapp/projections/EmbedEmployeesInProjects.java
+++ src/main/java/com/personal/example/employeeapp/projections/EmbedEmployeesInProjects.java
@@ -1,10 +1,10 @@
 package com.personal.example.employeeapp.projections;
-import com.personal.example.employeeapp.models.Department;
+import com.personal.example.employeeapp.models.Project;
 import org.springframework.data.rest.core.config.Projection;
 import java.util.List;
 
-@Projection(name = "embedEmployees", types = Department.class)
-public interface EmbedEmployees {
+@Projection(name = "embedEmployeesInProjects", types = Project.class)
+public interface EmbedEmployeesInProjects {
     String getName();
     List getEmployees();
 }
--- src/main/java/com/personal/example/employeeapp/repositories/ProjectRepository.java
+++ src/main/java/com/personal/example/employeeapp/repositories/ProjectRepository.java
@@ -1,10 +1,12 @@
 package com.personal.example.employeeapp.repositories;
 
 import com.personal.example.employeeapp.models.Project;
+import com.personal.example.employeeapp.projections.EmbedEmployeesInProjects;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(excerptProjection = EmbedEmployeesInProjects.class)
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
 }
